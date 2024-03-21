b1e9fe0e89af6ecf2adc0ea03b6363bb1b8db58b
Monika Wrona <mnkwrona@gmail.com>
2024-03-21 14:51:23 +0100

    add color select component

--- src/components/GSColorSelect.vue
+++ src/components/GSColorSelect.vue
@@ -1,3 +1,34 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type Color from '@/types/Color'
 
-<template></template>
+const props = defineProps<{
+  selected: Color
+  options: Color[]
+}>()
+
+const emit = defineEmits(['selected'])
+
+const styles = (option: Color): object => {
+  return {
+    'background-color': `var(--color-select-${option})`,
+    'border-color':
+      props.selected === option ? 'var(--color-select-selected)' : `var(--gs-c-${option})`
+  }
+}
+
+const handleClick = (option: Color) => {
+  emit('selected', option)
+}
+</script>
+
+<template>
+  <div class="gs-color-select flex flex-row gap-[4px]">
+    <div
+      v-for="(option, index) in options"
+      :key="index"
+      class="h-[28px] w-[28px] border-solid border-[3px] hover:opacity-80"
+      :style="styles(option)"
+      @click="handleClick(option)"
+    ></div>
+  </div>
+</template>
--- src/components/GSProductWidget.vue
+++ src/components/GSProductWidget.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 import type Widget from '@/types/Widget'
+import type Color from '@/types/Color'
 
 import GSCheckbox from './GSCheckbox.vue'
 import GSToggle from './GSToggle.vue'
+import GSColorSelect from './GSColorSelect.vue'
 
 defineProps<{
   widget: Widget
 }>()
+
+const colors: Color[] = ['blue', 'green', 'beige', 'white', 'black']
 </script>
 
 <template>
   <div class="gs-product-widget">
     <!-- <GSToggle /> -->
-    <GSCheckbox />
+    <!-- <GSCheckbox /> -->
+    <GSColorSelect :options="colors" selected="beige" />
   </div>
 </template>
